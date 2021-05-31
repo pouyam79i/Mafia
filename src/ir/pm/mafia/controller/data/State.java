a12d8619df83a9f4679564d60902d5e212f559cb
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-01 00:48:31 +0430

    State contains possible states of game current! ---> controller.data package

---
+++ src/ir/pm/mafia/controller/data/State.java
@@ -0,0 +1,19 @@
+package ir.pm.mafia.controller.data;
+
+import java.io.Serializable;
+
+/**
+ * State Contains state of game!
+ * State tells that where are we!
+ * For Example if we are still in lobby,
+ * the state is 'Lobby'.
+ * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
+ * @version v1.0
+ */
+public enum State implements Serializable {
+    Day,            // Day is when we display day chat room.
+    Vote,           // Vote happens after day. time to vote!
+    Night,          // Night is when you can do your action!
+    NightChat,      // (Only for mafia), display a chat room for mafia
+    Lobby           // Lobby is when the game is not started!
+}
