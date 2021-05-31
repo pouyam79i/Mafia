0f6b9109049359e9867e2947c0fe03c47ff35578
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-01 00:10:29 +0430

    Structure of shared memory is build! but not tested or debuged! ---> from controller.data package

---
+++ src/ir/pm/mafia/controller/data/SharedMemory.java
@@ -0,0 +1,71 @@
+package ir.pm.mafia.controller.data;
+
+import java.util.LinkedList;
+
+/**
+ * This class contains the structure of shared memory!
+ * Used between to transfer data between threads!
+ * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
+ * @version 1.0
+ */
+public class SharedMemory {
+
+    /**
+     * delete modes define the way of getting data from memory
+     * if true, data will be removed after getting it!
+     * if false, it reads last-in data
+     */
+    private final boolean deleteMode;
+    /**
+     * This is the shared memory structure!
+     * Which is a LinkedList at this moment!
+     */
+    private final LinkedList<DataBox> boxes;
+    private int lastReadIndex;
+
+    /**
+     * Constructor of SharedMemory.
+     * Setup the box of memory.
+     * @param deleteMode will be set as delete mode of shared memory
+     */
+    public SharedMemory(boolean deleteMode){
+        // Constructing a linked list!
+        boxes = new LinkedList<DataBox>();
+        this.deleteMode = deleteMode;
+        lastReadIndex = 0;
+    }
+
+    /**
+     * This method helps to get data from shared memory,
+     * in a synchronized way.
+     * It reads from the first-in data!
+     * If delete mode is true:
+     * after reading DataBox, it will delete it from shared memory!
+     * Else:
+     * it just reads new data and returns new data to user!
+     * @return DataBox
+     */
+    public synchronized DataBox get(){
+        DataBox dataBox = null;
+        if(boxes.size() > lastReadIndex)
+            if(deleteMode)
+                dataBox = boxes.remove(0);
+            else{
+                dataBox = boxes.get(lastReadIndex);
+                lastReadIndex++;
+            }
+        return dataBox;
+    }
+
+    /**
+     * This method helps to put (append) data into shared memory,
+     * in a synchronized way.
+     * It adds new data as last data.
+     * @param newBox will be added.
+     */
+    public synchronized void put(DataBox newBox){
+        if(newBox != null)
+            boxes.add(newBox);
+    }
+
+}
