71c13cd2951ea2ae3f451aba415ab90eaf00ad34
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-02 13:59:13 +0430

    Building client, it handles the connection from client to server ---> client.Client.java

--- src/ir/pm/mafia/controller/client/Client.java
+++ src/ir/pm/mafia/controller/client/Client.java
@@ -1,4 +1,84 @@
 package ir.pm.mafia.controller.client;
 
-public class Client {
+import ir.pm.mafia.controller.communication.Receive;
+import ir.pm.mafia.controller.communication.Send;
+import ir.pm.mafia.controller.data.SharedMemory;
+import ir.pm.mafia.model.utils.logger.LogLevel;
+import ir.pm.mafia.model.utils.logger.Logger;
+import ir.pm.mafia.model.utils.multithreading.Runnable;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * This class builds a connection from client to server
+ * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
+ * @version 1.1
+ */
+public class Client extends Runnable {
+
+    /**
+     * network socket
+     */
+    private final Socket socket;
+    /**
+     * sender handles sending process
+     */
+    private final Send sender;
+    /**
+     * receiver handles receiving process
+     */
+    private final Receive receiver;
+
+    /**
+     * Client constructor.
+     * Builds a safe connection to server.
+     * Sets requirements.
+     * @param ip IP of server
+     * @param port port of server
+     * @param sendBox shared memory of send box
+     * @param receiveBox shared memory of receive box
+     * @throws Exception if we have null input or failed to build a connection
+     */
+    public Client(String ip, int port,SharedMemory sendBox, SharedMemory receiveBox) throws Exception {
+        try {
+            if(ip == null || sendBox == null || receiveBox == null)
+                throw new IOException("Null input");
+            socket = new Socket(ip, port);
+            sender = new Send(sendBox, new ObjectOutputStream(socket.getOutputStream()));
+            receiver = new Receive(receiveBox, new ObjectInputStream(socket.getInputStream()));
+        }catch (Exception e){
+            Logger.error("Client constructor failed!: " + e.getMessage(),
+                    LogLevel.ClientFailed, "client.Client");
+            throw new Exception("Failed to build a connection");
+        }
+    }
+
+    /**
+     * Runs the client!
+     * Begins connection to server!
+     */
+    @Override
+    public void run() {
+        Thread send = new Thread(sender);
+        Thread receive = new Thread(receiver);
+        send.start();
+        receive.start();
+        while (!finished) Thread.onSpinWait();
+        try {
+            sender.close();
+            receiver.close();
+            // Waiting 0.1 second to make sure interruption does not happen
+            Thread.sleep(100);
+            socket.close();
+        } catch (Exception e) {
+            Logger.error("Failed to close client properly" + e.getMessage(),
+                    LogLevel.ClientDisconnected,
+                    "client.Client");
+        }
+        done = true;
+    }
+
 }
