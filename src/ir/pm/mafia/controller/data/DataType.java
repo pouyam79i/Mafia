a2d8a86c4f4109af671ed72de039cb9cdcb370d9
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-02 17:14:14 +0430

    Building DataType class to define transmitable datas ---> controller.data.DataType.java

---
+++ src/ir/pm/mafia/controller/data/DataType.java
@@ -0,0 +1,14 @@
+package ir.pm.mafia.controller.data;
+
+/**
+ * This class contain data type!
+ * Used to determine data types!
+ * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
+ * @version 1.0
+ */
+public enum DataType {
+
+    Message,        // This type is used for chat rooms
+    Undefined,      // This type is ignored!
+
+}
