5979d8df137424224afc8ecc14f009fa62874d78
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-05 02:36:07 +0430

    Building this launcher in test mode. (V1.0 not completed---> ChatRoom test succeded

--- src/ir/pm/mafia/model/launchers/AdminLauncher.java
+++ src/ir/pm/mafia/model/launchers/AdminLauncher.java
@@ -1,5 +1,17 @@
 package ir.pm.mafia.model.launchers;
 
+import ir.pm.mafia.controller.client.Client;
+import ir.pm.mafia.controller.server.Server;
+import ir.pm.mafia.model.loops.godloop.GodLoop;
+import ir.pm.mafia.model.player.Player;
+import ir.pm.mafia.model.utils.logger.LogLevel;
+import ir.pm.mafia.model.utils.logger.Logger;
+import ir.pm.mafia.view.console.Color;
+import ir.pm.mafia.view.console.Console;
+import ir.pm.mafia.view.ui.interfaces.ChatRoomUI;
+
+import java.util.Locale;
+
 /**
  * This Launcher is used to build a host for game,
  * and sets your account as admin of host,
@@ -7,13 +19,105 @@ package ir.pm.mafia.model.launchers;
  * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
  * @version 1.0
  */
-public class AdminLauncher implements Launcher {
+public class AdminLauncher implements Launcher, Color {
 
     /**
      * it launches a host and set you as admin player!
      */
     @Override
     public void launch() {
+
+        // Allocation memory
+        Console console = Console.getConsole();
+        String input = null;
+        console.println(GREEN + "Lunching Game ...");
+
+        // Building a player
+        Player player = new Player();
+
+        // Setting player name
+        player.setNickname(null);
+
+        // Building server!
+        Server server = null;
+        console.println(YELLOW + "Building host ...\n");
+        console.println(YELLOW + "Please enter a port number:");
+        console.println(PURPLE + "  -Like: 8000");
+        console.println(PURPLE + "  -If you want to leave enter '" + RED + "exit" + PURPLE + "'");
+        while (true){
+            try {
+                input = console.readConsole();
+                if(input.toLowerCase(Locale.ROOT).equals("exit")){
+                    console.println("Closing Launcher...");
+                    return;
+                }
+                server = new Server(Integer.parseInt(input));
+                break;
+            }catch (Exception e){
+                Logger.error("Failed to build server!" + e.getMessage(),
+                        LogLevel.ServerFailed, "AdminLauncher");
+                console.println(RED + "Invalid port!");
+            }
+        }
+        // setting player as server admin!
+        server.setAdmin(player);
+        // Running server!
+        server.start();
+
+        // Building client
+        Client client = null;
+        for(int i = 0; i < 10; i++){
+            try {
+                Thread.sleep(100);
+                client = new Client("127.0.0.1",
+                        server.getPort(),
+                        player.getSendBox(),
+                        player.getReceiveBox(),
+                        player.getToken());
+                break;
+            } catch (Exception e) {
+                if(i == 9){
+                    Logger.error("Launcher failed, cannot join server",
+                            LogLevel.ClientFailed,
+                            "AdminLauncher");
+                    console.println(RED + "Launcher failed!");
+                    server.shutdown();
+                    return;
+                }
+            }
+        }
+        // Running client
+        client.start();
+        console.println(GREEN + "You joined the host as admin :)");
+
+        // Running god loop
+        GodLoop godLoop;
+        try {
+            godLoop = new GodLoop(server.getConnectionBox(), player.getToken());
+            godLoop.start();
+        } catch (Exception e) {
+            console.println(RED + "Launcher failed!");
+            return;
+        }
+
+        //  Test area *******************************************************
+        try {
+            ChatRoomUI chatRoomUI = new ChatRoomUI(player.getSendBox(), player.getReceiveBox(),
+                    player.getToken(), player.getNickname(), BLUE + "Lobby" + RESET);
+            chatRoomUI.start();
+        } catch (Exception ignored) {}
+
+        // Wait 3 min
+        try {
+            Thread.sleep(180000);
+        } catch (InterruptedException ignored) {}
+        //  End of test area *******************************************************
+
+
+        // close area
+        godLoop.shutdown();
+        server.shutdown();
+        client.shutdown();
 
     }
 
--- src/ir/pm/mafia/model/launchers/PlayerLauncher.java
+++ src/ir/pm/mafia/model/launchers/PlayerLauncher.java
@@ -1,18 +1,87 @@
 package ir.pm.mafia.model.launchers;
 
+import ir.pm.mafia.controller.client.Client;
+import ir.pm.mafia.model.player.Player;
+import ir.pm.mafia.model.utils.logger.LogLevel;
+import ir.pm.mafia.model.utils.logger.Logger;
+import ir.pm.mafia.view.console.Color;
+import ir.pm.mafia.view.console.Console;
+import ir.pm.mafia.view.ui.interfaces.ChatRoomUI;
+
+import java.util.Locale;
+
 /**
  * This launcher help you to build a connection to server,
  * then runs the game
  * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
  * @version 1.0
  */
-public class PlayerLauncher implements Launcher {
+public class PlayerLauncher implements Launcher, Color {
 
     /**
      * Lunches client-side. runs the game for player!
      */
     @Override
     public void launch() {
+
+        // Allocation memory
+        Console console = Console.getConsole();
+        String input = null;
+        console.println(GREEN + "Lunching Game ...");
+
+        // Building a player
+        Player player = new Player();
+
+        // Setting player name
+        player.setNickname(null);
+
+        // Building Client, connection to server
+        boolean breakLoop = false;
+        Client client = null;
+        console.println(YELLOW + "Inter IP:Port of server:");
+        console.println(PURPLE + "  -Like: 127.0.0.1:8000");
+        console.println(PURPLE + "  -If you want to leave enter '" + RED + "exit" + PURPLE + "'");
+        while (!breakLoop){
+            input = console.readConsole();
+            // check if exit is called!
+            if(input.toLowerCase(Locale.ROOT).equals("exit")){
+                console.println(YELLOW + "Closing launcher");
+                return;
+            }
+            try {
+                String ip = input.split(":")[0];
+                int port = Integer.parseInt(input.split(":")[1]);
+                client = new Client(ip, port, player.getSendBox(), player.getReceiveBox(), null);
+                console.println(GREEN + "Found Server");
+                console.println(YELLOW + "Running connection...");
+                client.start();
+                breakLoop = true;
+            }catch (Exception e){
+                Logger.error("Failed to build client" + e.getMessage(),
+                        LogLevel.ClientFailed,
+                        "Launcher");
+                console.println(RED + "Invalid IP:Port!");
+            }
+        }
+        player.setToken(client.getMyToken());
+        console.println(GREEN + "You joined server successfully!");
+
+        //  Test area *******************************************************
+        try {
+            ChatRoomUI chatRoomUI = new ChatRoomUI(player.getSendBox(), player.getReceiveBox(),
+                    player.getToken(), player.getNickname(), BLUE + "Lobby" + RESET);
+            chatRoomUI.start();
+        } catch (Exception ignored) {}
+
+        // Wait 3 min
+        try {
+            Thread.sleep(180000);
+        } catch (InterruptedException ignored) {}
+        //  End of test area *******************************************************
+
+
+        // close area
+        client.shutdown();
 
     }
 
