97baf13daa64d128fc77700b5d963862a95ff248
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-04 15:23:14 +0430

    Creating PlayerLauncher, used to launch in normal player mode and join a host

---
+++ src/ir/pm/mafia/model/launchers/PlayerLauncher.java
@@ -0,0 +1,19 @@
+package ir.pm.mafia.model.launchers;
+
+/**
+ * This launcher help you to build a connection to server,
+ * then runs the game
+ * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
+ * @version 1.0
+ */
+public class PlayerLauncher implements Launcher {
+
+    /**
+     * Lunches client-side. runs the game for player!
+     */
+    @Override
+    public void launch() {
+
+    }
+
+}
