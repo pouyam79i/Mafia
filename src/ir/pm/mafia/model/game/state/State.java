07c40fad113b77ba0c5c26d8d9d157ed0b26b89c
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-05 02:16:14 +0430

    Adding Initial as last added state. upgraded to v1.1

--- src/ir/pm/mafia/model/game/state/State.java
+++ src/ir/pm/mafia/model/game/state/State.java
@@ -8,12 +8,15 @@ import java.io.Serializable;
  * For Example if we are still in lobby,
  * the state is 'Lobby'.
  * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
- * @version v1.0
+ * @version v1.1
  */
 public enum State implements Serializable {
+
     Day,            // Day is when we display day chat room.
-    Vote,           // Vote happens after day. time to vote!
-    Night,          // Night is when you can do your action!
-    NightChat,      // (Only for mafia), display a chat room for mafia
-    Lobby           // Lobby is when the game is not started!
+    Vote,           // Vote happens after day. time to vote.
+    Night,          // Night is when you can do your action.
+    NightChat,      // (Only for mafia), display a chat room for mafia!
+    Lobby,          // Lobby is when the game is not started.
+    Initial,        // When no state is set and good loob is just created.
+
 }
