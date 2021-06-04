170ad2cea3e43b50154e46afdd5cf75301e8894f
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-04 15:18:35 +0430

    Contains sructure of all Launchers! ---> Launcher.java

---
+++ src/ir/pm/mafia/model/launchers/Launcher.java
@@ -0,0 +1,15 @@
+package ir.pm.mafia.model.launchers;
+
+/**
+ * This class contains the structure of all launchers!
+ * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
+ * @version 1.0
+ */
+public interface Launcher {
+
+    /**
+     * This method is used to run the launcher!
+     */
+    void launch();
+
+}
