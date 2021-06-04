69f11961128f515cb38ebee1b4f19d599c1ccf80
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-04 15:44:06 +0430

    Adding LauncherFailed to LogLevel. upgraded to v1.1.1

--- src/ir/pm/mafia/model/utils/logger/LogLevel.java
+++ src/ir/pm/mafia/model/utils/logger/LogLevel.java
@@ -3,7 +3,7 @@ package ir.pm.mafia.model.utils.logger;
 /**
  * This class contains log level of this program 'Mafia'
  * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
- * @version 1.1
+ * @version 1.1.1
  */
 public enum LogLevel {
 
@@ -17,5 +17,6 @@ public enum LogLevel {
     ClientDisconnected,     // When client connection or connection to client ends.
     ShutdownCall,           // When we want to shutdown a service.
     Report,                 // When we just want to log something.
+    LauncherFailed,         // When launcher is failed to create a game.
 
 }
