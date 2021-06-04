320ffe1d19a6164dc6671b06f449b5b19d83a588
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-04 23:03:29 +0430

    Building SenderHandler to be able to handle Sending  data box from part of game(part handler), in a multi thread way ---> SenderHandler.java

---
+++ src/ir/pm/mafia/model/loops/godloop/SenderHandler.java
@@ -0,0 +1,69 @@
+package ir.pm.mafia.model.loops.godloop;
+
+import ir.pm.mafia.controller.data.DataBase;
+import ir.pm.mafia.controller.data.DataBox;
+import ir.pm.mafia.controller.server.ClientHandler;
+import ir.pm.mafia.model.utils.multithreading.Runnable;
+
+/**
+ * This Class handles sending data from server to client
+ * while client must read same data box!
+ * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
+ * @version 1.0
+ */
+public class SenderHandler extends Runnable {
+
+    /**
+     * contains a connection
+     */
+    private final ClientHandler clientHandler;
+    /**
+     * is the token of this connection!
+     */
+    private final String clientToken;
+    /**
+     * shared data base! client must read its data!
+     */
+    private final DataBase sharedSendBox;
+    /**
+     * last read index of data base
+     */
+    private int lastRead;
+
+    /**
+     * Setup a sending connection to client!
+     * @param clientHandler contains connection property
+     * @param sharedSendBox is the shared data base! client reads this
+     * @throws Exception if failed to build a proper SenderHandler!
+     */
+    public SenderHandler(ClientHandler clientHandler, DataBase sharedSendBox) throws Exception {
+        if(clientHandler == null || sharedSendBox == null)
+            throw new Exception("Null input");
+        this.clientHandler = clientHandler;
+        this.sharedSendBox = sharedSendBox;
+        clientToken = clientHandler.getToken();
+        lastRead = 0;
+    }
+
+    /**
+     * Runs a Sender handler!
+     */
+    @Override
+    public void run() {
+        while (clientHandler.isConnected()){
+            while (lastRead < sharedSendBox.getSize()){
+                DataBox dataBox = (DataBox) sharedSendBox.readData(lastRead);
+                if(!dataBox.getData().getSenderToken().equals(clientToken))
+                    clientHandler.send(dataBox);
+                lastRead++;
+            }
+        }
+        this.close();
+    }
+
+    //Getters
+    public ClientHandler getClientHandler() {
+        return clientHandler;
+    }
+
+}
