7be6ffe5b11ac1f11a757d42f2a4ef6f6bebab87
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-07 12:50:50 +0430

    Building GodLoop until Lobby

--- src/ir/pm/mafia/model/loops/godloop/GodLoop.java
+++ src/ir/pm/mafia/model/loops/godloop/GodLoop.java
@@ -1,17 +1,14 @@
 package ir.pm.mafia.model.loops.godloop;
 
-//import ir.pm.mafia.controller.data.Data;
-//import ir.pm.mafia.controller.data.DataBase;
 import ir.pm.mafia.controller.data.SharedMemory;
 import ir.pm.mafia.controller.server.ClientHandler;
-import ir.pm.mafia.model.game.handlers.Lobby;
+import ir.pm.mafia.model.game.handlers.logic.lobby.Lobby;
 import ir.pm.mafia.model.game.handlers.PartHandler;
 import ir.pm.mafia.model.game.state.State;
 import ir.pm.mafia.model.game.state.StateUpdater;
 import ir.pm.mafia.model.utils.multithreading.Runnable;
 
 import java.util.ArrayList;
-//import java.util.Iterator;
 
 /**
  * This Class is the server main loop so it is called GodLoop!
@@ -36,24 +33,11 @@ public class GodLoop extends Runnable {
      * Contains connected client handlers!
      */
     private ArrayList<ClientHandler> currentConnections;
-//    /**
-//     * Receiver Handler list.
-//     * These are used to read data from clients,
-//     * then these data will be saved in data base and part handler,
-//     * will analyze that.
-//     */
-//    private ArrayList<ReceiverHandler> receiverHandlers;
     /**
      * Current part handler,
      * contains a logic analyzer for current state of game!
      */
     private PartHandler currentPart;
-//    /**
-//     * This will be given to part handlers!
-//     * received data will be analyzed, and gathered
-//     * in this data base!
-//     */
-//    private DataBase currentDataBase;
     /**
      * Game stated state!
      * if true it means game has started!
@@ -63,12 +47,6 @@ public class GodLoop extends Runnable {
      * Token of admin will be used to confirm admin commands!
      */
     private final String adminToken;
-//    /**
-//     * Input box is where add of received data boxes
-//     * have been gathered.
-//     * It has the SharedMemory structure.
-//     */
-//    private final SharedMemory inputBox;
     /**
      * In this shared memory we pull new list of client handlers.
      */
@@ -87,9 +65,6 @@ public class GodLoop extends Runnable {
         this.connectionBox = connectionBox;
         this.adminToken = adminToken;
         currentConnections = new ArrayList<ClientHandler>();
-//        receiverHandlers = new ArrayList<ReceiverHandler>();
-//        inputBox = new SharedMemory(true);
-//        currentDataBase = new DataBase();
         stateUpdater = new StateUpdater();
         currentState = State.Initial;
         gameStarted = false;
@@ -110,65 +85,16 @@ public class GodLoop extends Runnable {
         if(newConnectionList == null)
             return;
         currentConnections = newConnectionList;
-//        if(newConnectionList != null){
-//            currentConnections = newConnectionList;
-//            for(ClientHandler clientHandler : currentConnections){
-//                boolean exists = false;
-//                // Debug this shit #########################################################################
-//                for(ReceiverHandler receiverHandler : receiverHandlers){
-//                    if (receiverHandler.getClientHandler() == clientHandler) {
-//                        exists = true;
-//                        break;
-//                    }
-//                }
-//                if(!exists){
-//                    try {
-//                        ReceiverHandler newReceiverHandler = new ReceiverHandler(clientHandler, inputBox);
-//                        newReceiverHandler.start();
-//                        receiverHandlers.add(newReceiverHandler);
-//                    } catch (Exception ignored) {}
-//                }
-//            }
-//        }
-//        refreshReceiverHandlerList();
     }
-
-//    /**
-//     * It removes disconnected or finished receivers!
-//     */
-//    public void refreshReceiverHandlerList(){
-//        if(gameStarted)
-//            return;
-//        Iterator<ReceiverHandler> rh = receiverHandlers.iterator();
-//        while(rh.hasNext()){
-//            ReceiverHandler cRH = rh.next();
-//            if(!currentConnections.contains(cRH.getClientHandler()))
-//                cRH.shutdown();
-//                rh.remove();
-//        }
-//    }
 
     /**
      * When admin calls to start the game!
      * It will lock every thing :)
      */
     public void startTheGame(){
-//        updateConnections();
+        updateConnections();
         gameStarted = true;
     }
-
-//    /**
-//     * adds new data to data base!
-//     * if game is not started, it will check admins commands
-//     */
-//    public void updateDateBase(){
-//        while (true){
-//            Data newData = (Data) inputBox.get();
-//            if(newData == null)
-//                break;
-//            currentDataBase.add(newData);
-//        }
-//    }
 
     /**
      * Updates the part handler for the current sate of game
@@ -179,8 +105,6 @@ public class GodLoop extends Runnable {
         // If state is not change it does nothing
         if(state == currentState)
             return;
-//        // Building new data base for new part!
-//        currentDataBase = new DataBase();
 
         // Shutting down current part
         if(currentPart != null)
@@ -193,11 +117,10 @@ public class GodLoop extends Runnable {
         if(state == State.Lobby){
             try {
                 gameStarted = false;
-//                Lobby newLobby = new Lobby(currentDataBase);
-                Lobby newLobby = new Lobby();
+                Lobby newLobby = new Lobby(adminToken);
                 newLobby.setLock(gameStarted);
-                newLobby.updateClientHandlers(currentConnections);
                 newLobby.start();
+                newLobby.updateClientHandlers(currentConnections);
                 currentPart = newLobby;
             } catch (Exception ignored) {}
         }
@@ -221,7 +144,7 @@ public class GodLoop extends Runnable {
 
     /**
      * This is the game loop of server!
-     * Called god of game
+     * Called god of game.
      */
     @Override
     public void run() {
@@ -230,7 +153,6 @@ public class GodLoop extends Runnable {
             updateConnections();
             updatePartHandler(stateUpdater.getCurrentState());
             currentPart.updateClientHandlers(currentConnections);
-//            updateDateBase();
         }
     }
 
@@ -239,6 +161,7 @@ public class GodLoop extends Runnable {
      */
     @Override
     public void shutdown(){
+        finished = true;
         if(stateUpdater != null)
             stateUpdater.shutdown();
         if(currentPart != null)
