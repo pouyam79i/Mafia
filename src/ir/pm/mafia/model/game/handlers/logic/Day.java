ec1ddfead20aa3fa65c477c4d32e882f71aa411f
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-09 02:09:04 +0430

    Creating Day and Night and Vote class to handler the logic of game. ---> in game.logic package

---
+++ src/ir/pm/mafia/model/game/handlers/logic/Day.java
@@ -0,0 +1,20 @@
+package ir.pm.mafia.model.game.handlers.logic;
+
+import ir.pm.mafia.controller.data.DataBase;
+import ir.pm.mafia.model.game.handlers.PartHandler;
+
+public class Day extends PartHandler {
+
+    public Day() {
+    }
+
+    @Override
+    protected void applyLogic() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/ir/pm/mafia/model/game/handlers/logic/Vote.java
@@ -0,0 +1,20 @@
+package ir.pm.mafia.model.game.handlers.logic;
+
+import ir.pm.mafia.controller.data.DataBase;
+import ir.pm.mafia.model.game.handlers.PartHandler;
+
+public class Vote extends PartHandler {
+
+    public Vote() {
+    }
+
+    @Override
+    protected void applyLogic() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
