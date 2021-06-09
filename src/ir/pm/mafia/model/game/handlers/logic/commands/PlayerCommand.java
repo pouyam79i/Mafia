d337581fc4b34dd8aea84aaf1bcb5ed29b15569a
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-09 14:58:17 +0430

    Building commands for player that they can used it during the game

---
+++ src/ir/pm/mafia/model/game/handlers/logic/commands/PlayerCommand.java
@@ -0,0 +1,15 @@
+package ir.pm.mafia.model.game.handlers.logic.commands;
+
+/**
+ * This enum contains normal player commands!
+ * can be used during the game
+ * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
+ * @version 1.0
+ */
+public enum PlayerCommand {
+
+    CLOSE,          // Closes the game loop and server.
+    LIVES,          // Returns number of current connections.
+    CONFIRM,        // Confirmation to do changes.
+
+}
