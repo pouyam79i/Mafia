b00a2f3f660e93cfc9d006c305d10568985ac638
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-09 23:16:22 +0430

    Building Group enum to define group of players

--- src/ir/pm/mafia/model/game/character/Group.java
+++ src/ir/pm/mafia/model/game/character/Group.java
@@ -1,5 +1,12 @@
 package ir.pm.mafia.model.game.character;
 
+/**
+ * There Are two group of player!
+ * Mafia and Citizen
+ * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
+ * @version v1.0
+ */
 public enum Group {
-    Citizen, Mafia
+    Citizen,    // Normal people
+    Mafia,      // Mafia group
 }
