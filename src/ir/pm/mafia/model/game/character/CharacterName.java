fb22e8271b034a7ba941c60a2f81bd3d860ecb7d
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-10 15:10:00 +0430

    Building CharacterName, it contains each characters name

---
+++ src/ir/pm/mafia/model/game/character/CharacterName.java
@@ -0,0 +1,24 @@
+package ir.pm.mafia.model.game.character;
+
+/**
+ * This enum contains name of characters.
+ * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
+ * @version v1.0
+ */
+public enum CharacterName {
+
+    // Mafia
+    GodFather,
+    DoctorLecter,
+    NormalMafia,
+
+    // Citizen
+    Mayer,
+    DoctorCitizen,
+    Detective,
+    Sniper,
+    Psychologist,
+    Impregnable,
+    NormalCitizen
+
+}
