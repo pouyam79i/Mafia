e7f71f17dd2f92a123ea55dfc3b208a941ef2d69
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-10 15:12:33 +0430

    Adding STARTED to game feature. upgraded to v1.1.2

--- src/ir/pm/mafia/model/game/state/State.java
+++ src/ir/pm/mafia/model/game/state/State.java
@@ -8,16 +8,16 @@ import java.io.Serializable;
  * For Example if we are still in lobby,
  * the state is 'Lobby'.
  * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
- * @version v1.1.1
+ * @version v1.1.2
  */
 public enum State implements Serializable {
 
     Day,            // Day is when we display day chat room.
     Vote,           // Vote happens after day. time to vote.
     Night,          // Night is when you can do your action.
-    NightChat,      // (Only for mafia), display a chat room for mafia!
     Lobby,          // Lobby is when the game is not started.
     Initial,        // When no state is set and good loop is just created.
-    FINISHED,       // When game finished, show the winner!
+    FINISHED,       // When game finished, show the winner.
+    STARTED,        // When game has just started. Runs the introduction night!
 
 }
