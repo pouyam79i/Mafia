4a740ae61fede95f94602007d239302998d37e40
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-10 15:03:07 +0430

    Building CharacterFactory it is the character builder, used in the introduction night to set characters

--- src/ir/pm/mafia/model/game/character/CharacterFactory.java
+++ src/ir/pm/mafia/model/game/character/CharacterFactory.java
@@ -1,4 +1,78 @@
 package ir.pm.mafia.model.game.character;
 
+import ir.pm.mafia.model.game.character.characters.*;
+
+/**
+ * This class builds character for us! (Contains Factory Method)
+ * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
+ * @version v1.0
+ */
 public class CharacterFactory {
+
+    /**
+     * Number of mafia that has been build!
+     */
+    private int mafia;
+    /**
+     * Number of citizen that has been build!
+     */
+    private int citizen;
+
+    /**
+     * Constructor of CharacterFactory
+     * Sets built character to zero!
+     */
+    public CharacterFactory(){
+        mafia = 0;
+        citizen = 0;
+    }
+
+    /**
+     * Factory method to build character
+     * @param group of wanted characters!
+     * @return Character
+     */
+    public Character buildCharacter(Group group){
+        Character character = null;
+        // building for mafia group
+        if(group == Group.Citizen){
+            character = switch (mafia) {
+                case 0 -> new Godfather();
+                case 1 -> new DoctorLecter();
+                default -> new NormalMafia();
+            };
+            mafia++;
+        }
+        // building for citizen group
+        else if(group == Group.Mafia){
+            character = switch (citizen) {
+                case 0 -> new Mayer();
+                case 1 -> new DoctorCitizen();
+                case 2 -> new Sniper();
+                case 3 -> new Detective();
+                case 4 -> new Psychologist();
+                case 5 -> new Impregnable();
+                default -> new NormalCitizen();
+            };
+            citizen++;
+        }
+        return character;
+    }
+
+    /**
+     * Resets built character number!
+     */
+    public void reset(){
+        mafia = 0;
+        citizen = 0;
+    }
+
+    // Getters!
+    public int getMafia() {
+        return mafia;
+    }
+    public int getCitizen() {
+        return citizen;
+    }
+
 }
