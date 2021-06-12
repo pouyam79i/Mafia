688aeabddeb27c245ebeb62d131b510a862a4072
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-13 04:24:26 +0430

    Adding new game commands! Last command is CLEAR. upgraded to v1.1

--- src/ir/pm/mafia/model/game/logic/commands/PlayerCommand.java
+++ src/ir/pm/mafia/model/game/logic/commands/PlayerCommand.java
@@ -4,12 +4,16 @@ package ir.pm.mafia.model.game.logic.commands;
  * This enum contains normal player commands!
  * can be used during the game
  * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
- * @version 1.0
+ * @version 1.1
  */
 public enum PlayerCommand {
 
-    CLOSE,          // Closes the game loop and server.
+    CLOSE,          // Closes the game loop.
     LIVES,          // Returns number of current connections.
-    CONFIRM,        // Confirmation to do changes.
+    CONFIRM,        // Confirmation to do changes
+    VOTE,           // Vote some body.
+    ACTION,         // Preform your action.
+    CLEAR,          // Clear decided vote or action!
+    DISMISS,        // Works only for mayer!
 
 }
