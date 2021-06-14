af34aa2775d68d38dc7e5315fee94a12f6fffa79
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-14 06:31:29 +0430

    Adding Action and Vote to dataTypes. upgraded to v1.0.2

--- src/ir/pm/mafia/controller/data/DataType.java
+++ src/ir/pm/mafia/controller/data/DataType.java
@@ -4,11 +4,13 @@ package ir.pm.mafia.controller.data;
  * This class contain data type!
  * Used to determine data types!
  * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
- * @version 1.0.1
+ * @version 1.0.2
  */
 public enum DataType {
 
-    Message,        // This type is used for chat rooms
+    Message,        // Used for chat rooms to transform text messages and commands.
+    Vote,           // Used to vote.
+    Action,         // Used for action.
     Undefined,      // This type is ignored!
 
 }
