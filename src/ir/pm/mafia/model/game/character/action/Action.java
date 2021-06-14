b5e5023d5d8e71024817d485019e2464d5fedf24
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-14 06:32:51 +0430

    Seperating shoot and revive action. upgraded to v1.1

--- src/ir/pm/mafia/model/game/character/action/Action.java
+++ src/ir/pm/mafia/model/game/character/action/Action.java
@@ -1,15 +1,17 @@
-package ir.pm.mafia.model.game.character;
+package ir.pm.mafia.model.game.character.action;
 
 /**
  * Contains character actions
  * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
- * @version 1.0
+ * @version 1.1
  */
 public enum Action {
 
-    SHOOT,          // Head of mafia and sniper.
-    REVIVE,         // Doctors can revive.
-    DISMISS,        // Mayer can dismiss voting.
+    SHOOT_MAFIA,    // Head of mafia can kill citizen.
+    SHOOT_SNIPER,   // Sniper can kill players.
+    REVIVE_MAFIA,   // Doctor lecter can revive mafia.
+    REVIVE,         // Citizen doctor can revive players.
+    DISMISS,        // Mayer can dismiss the voting.
     SILENCE,        // Psychologist can quiet one player.
     ENQUIRY,        // Detective can enquiry some character.
     ENQUIRY_DEAD,   // Impregnable can enquiry dead character.
