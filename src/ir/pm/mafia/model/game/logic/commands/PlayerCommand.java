11d0810eab0d7ddb1a89d9eb208322a284eec82b
pouyam79i <pouyamohammadyirbu@gmail.com>
2021-06-15 04:10:51 +0430

    Adding HISTORY command. upgrade to v1.1.2

--- src/ir/pm/mafia/model/game/logic/commands/PlayerCommand.java
+++ src/ir/pm/mafia/model/game/logic/commands/PlayerCommand.java
@@ -4,7 +4,7 @@ package ir.pm.mafia.model.game.logic.commands;
  * This enum contains normal player commands!
  * can be used during the game
  * @author Pouya Mohammadi - CE@AUT - Uni ID:9829039
- * @version 1.1.1
+ * @version 1.1.2
  */
 public enum PlayerCommand {
 
@@ -16,5 +16,6 @@ public enum PlayerCommand {
     CLEAR,          // Clear decided vote or action.
     DISMISS,        // Works only for mayer to dismiss a voting.
     SKIP,           // Used to skip a part like day chat room.
+    HISTORY         // Used to print previous chatroom information.
 
 }
